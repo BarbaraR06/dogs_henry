fef789ed381f48c8ac3e128dadb878222eb18571
Barbara <baarbi.sona@gmail.com>
2023-06-17 22:07:38 -0300

    barbi

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,43 +1,31 @@
-
 import React from "react";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { getName, setCurrentPage } from "../redux/actions";
 import styles from "../css/SearchBar.module.css";
-import { useHistory } from "react-router-dom";
 
 const SearchBar = () => {
-  const dispatch = useDispatch();
-  const [dog, setDog] = useState("");
-  const history = useHistory();
+    
+    const dispatch = useDispatch();
+    
+    const [ dog, setDog ] = useState("");
+    
+    const handleChange = (event) => {
+        dispatch (getName(event))
+        dispatch (setCurrentPage(1))
+    }
 
-  const handleChange = (event) => {
-    const searchQuery = event.target.value;
-    dispatch(getName(searchQuery));
-    dispatch(setCurrentPage(1));
 
-    // Cambiar la URL a la página de detalle con el ID del perro buscado
-    history.push(`/detail/${searchQuery}`);
-  };
 
-  const handleClick = (event) => {
-    setDog("");
-  };
-
-  return (
-    <div className={styles.container}>
-      <input
-        className={`${styles.input} ${styles["input-text-color"]}`}
-        type="text"
-        value={dog}
-        onChange={(event) => {
-          setDog(event.target.value);
-          handleChange(event);
-        }}
-        placeholder="Search by name"
-      />
-    </div>
-  );
-};
+   const handleClick = (event) => {
+       setDog ("")
+   }
+   
+    return (
+        <div className={styles.container}>
+          <input className={`${styles.input} ${styles['input-text-color']}`} type="text" value={dog} onChange={(event) => {setDog(event.target.value); handleChange(event.target.value)}} placeholder="Search by name"/>
+        </div>
+    )
+}
 
 export default SearchBar;
