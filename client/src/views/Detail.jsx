402f934ccbdee92b8bf70e05b038fbbe72c19db1
Barbara <baarbi.sona@gmail.com>
2023-06-19 19:35:29 -0300

    barbi

--- client/src/views/Detail.jsx
+++ client/src/views/Detail.jsx
@@ -4,28 +4,29 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import styles from "../css/Detail.module.css";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import NavBar from "../components/NavBar";
 
-const Detail = ({temperament}) => {
+const Detail = () => {
   const dispatch = useDispatch();
+  
   const { id } = useParams();
-
+  
+  
   useEffect(() => {
     dispatch(getDogDetail(id));
     return () => {
       dispatch(resetDetail());
     };
   }, [dispatch, id]);
-
+  
   const dogDetail = useSelector((state) => state.dogDetail);
-
+  
   if (Object.keys(dogDetail).length === 0) {
     return <div> Cargando... </div>;
   }
 
   const dog = Array.isArray(dogDetail) ? dogDetail[0] : dogDetail;
-
 
   return (
     <div className={styles.mainContainer + " " + styles.Detail}>
@@ -43,12 +44,11 @@ const Detail = ({temperament}) => {
           <h3>Id: {dog?.id}</h3>
           <h1>Breed: {dog?.name}</h1>
           <h3>Weight:</h3>
-          <span>Min: {dog?.weightMin}</span> -{" "}
-          <span>Max: {dog?.weightMax}</span>
+          <span>Min: {dog?.weightMin}</span> - <span>Max: {dog?.weightMax}</span>
           <h3>Average weight: {dog?.averageWeight}</h3>
           <h3>Height (min - max): {dog?.height.metric}</h3>
           <h3>Life expectancy: {dog?.life_span}</h3>
-          <h3> Temperaments: {temperament.name}</h3>
+          <h3>Temperament: {dog?.temperament.name}</h3>
         </div>
       </div>
     </div>
