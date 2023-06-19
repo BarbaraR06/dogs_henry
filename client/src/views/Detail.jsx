5825ff59883cadc18c9538a329167c503377bc2f
Barbara <baarbi.sona@gmail.com>
2023-06-19 19:22:09 -0300

    barbi

--- client/src/views/Detail.jsx
+++ client/src/views/Detail.jsx
@@ -4,35 +4,36 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import styles from "../css/Detail.module.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 const Detail = () => {
   const dispatch = useDispatch();
-  
   const { id } = useParams();
-  
-  
+
   useEffect(() => {
     dispatch(getDogDetail(id));
     return () => {
       dispatch(resetDetail());
     };
   }, [dispatch, id]);
-  
+
   const dogDetail = useSelector((state) => state.dogDetail);
-  
+
   if (Object.keys(dogDetail).length === 0) {
     return <div> Cargando... </div>;
   }
-  
+
   const dog = Array.isArray(dogDetail) ? dogDetail[0] : dogDetail;
   let temperaments = dog.temperament;
+  let isFromDatabase = false;
 
   // Verificar si el campo temperamento es un array
   if (Array.isArray(temperaments)) {
     temperaments = temperaments.join(", ");
+    isFromDatabase = true;
   }
+
   return (
     <div className={styles.mainContainer + " " + styles.Detail}>
       <NavBar className={styles.navBar} />
@@ -49,11 +50,16 @@ const Detail = () => {
           <h3>Id: {dog?.id}</h3>
           <h1>Breed: {dog?.name}</h1>
           <h3>Weight:</h3>
-          <span>Min: {dog?.weightMin}</span> - <span>Max: {dog?.weightMax}</span>
+          <span>Min: {dog?.weightMin}</span> -{" "}
+          <span>Max: {dog?.weightMax}</span>
           <h3>Average weight: {dog?.averageWeight}</h3>
           <h3>Height (min - max): {dog?.height.metric}</h3>
           <h3>Life expectancy: {dog?.life_span}</h3>
-          <h3>Temperament: {dog?.temperament}</h3>
+          {isFromDatabase ? (
+            <h3>Temperament from database: {temperaments}</h3>
+          ) : (
+            <h3>Temperament from API: {temperaments}</h3>
+          )}
         </div>
       </div>
     </div>
